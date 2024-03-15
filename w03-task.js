e43a5097d5bc1a23db4ed4b0185f344ebb1f7bbf
JamesStolte <jamesstolte0879@gmail.com>
2024-03-14 23:23:27 -0600

    stuff

--- w03-task.js
+++ w03-task.js
@@ -9,7 +9,7 @@ function add (number1, number2){
 function addNumbers () {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
-    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+    document.querySelector('#sum').value.innerHTML = add(addNumber1, addNumber2);
 }
 
 /* Function Expression - Subtract Numbers */
